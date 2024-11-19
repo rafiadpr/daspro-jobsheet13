9d504141944101c1ccf858028cb1c91e24fbb7a8
rafiadpr <work.rafiadrianp@gmail.com>
2024-11-19 20:49:42 +0700

    percobaan 3

---
+++ hitungTotalHarga23.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class hitungTotalHarga23 {
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
+        int pilihanMenu = input.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = input.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+    }
+}
