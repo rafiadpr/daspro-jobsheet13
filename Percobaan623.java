2172f5b57e2e88db7c84806ab8e5f91f2b4e2e0d
rafiadpr <work.rafiadrianp@gmail.com>
2024-11-19 21:10:34 +0700

    percobaan 6 - separated class

--- Percobaan623.java
+++ Percobaan623.java
@@ -3,22 +3,33 @@ import java.util.Scanner;
 
 public class Percobaan623 {
 
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
 
         int p, l, t, L, vol;
 
-        System.out.println("Masukkan panjang");
+        System.out.print("Masukkan panjang : ");
         p = input.nextInt();
-        System.out.println("Masukkan lebar");
+        System.out.print("Masukkan lebar : ");
         l = input.nextInt();
-        System.out.println("Masukkan tinggi");
+        System.out.print("Masukkan tinggi : ");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas Persegi Panjang adalah " + L);
 
-        vol = p * l * t;
+        vol = hitungVolume(t, p, L);
         System.out.println("Volume balok adalah " + vol);
     }
 }
