b8134b0797f6f80c7e92ef0c91300ea0c51283a9
rafiadpr <work.rafiadrianp@gmail.com>
2024-11-19 20:41:14 +0700

    percobaan 2

--- Kafe23.java
+++ Kafe23.java
@@ -1,7 +1,13 @@
 
 public class Kafe23 {
 
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 15,000");
@@ -14,6 +20,6 @@ public class Kafe23 {
     }
 
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 }
