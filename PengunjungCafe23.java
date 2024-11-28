4ddf4db1b95b862be0e3da237b58945e6457e6bf
rafiadpr <work.rafiadrianp@gmail.com>
2024-11-28 13:08:19 +0700

    Percobaan 4 no 2

--- PengunjungCafe23.java
+++ PengunjungCafe23.java
@@ -3,8 +3,8 @@ public class PengunjungCafe23 {
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("- " + pengunjung);
         }
     }
 
